5969ca81f4c5579983b09fcda018d979a9491c8a
Santiago <sanalba2222@gmail.com>
2022-04-03 19:28:17 -0300

    terminada mayoria de secciones

--- Trello/js/home.js
+++ Trello/js/home.js
@@ -7,3 +7,35 @@ window.addEventListener("scroll", () => {
     navbar.classList.remove("navbar-display");
   }
 });
+
+var learnMore = document.querySelectorAll(".learn-more");
+
+learnMore.forEach((item) => {
+  item.addEventListener("click", () => {
+    item.nextElementSibling.classList.toggle("expanded");
+    // item.nextElementSibling.style.height = "auto";
+  });
+});
+
+// var learn = document.querySelector(".learn-more");
+
+// learn.addEventListener("click", () => {
+//   learn.nextElementSibling.style.height = "auto";
+// });
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  // direction: "vertical",
+  // loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".button-next",
+    prevEl: ".button-prev",
+  },
+});
